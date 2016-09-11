e27304b3043be1f372eae56bab0acff8cae89726
RaddKat <mikeradka@gmail.com>
2016-09-10 21:19:48 -0700

    Added fiveTimes2.py

---
+++ ATBS/004-fiveTimes2.py
@@ -0,0 +1,9 @@
+# This program demonstrates the use of a while loop
+# This program outputs the same results as 003-fiveTimes.py, but by using a while loop instead of a for loop
+# Written in Python 3.5.1
+
+print('My name is')
+i = 0
+while i < 5:
+    print('Jimmy Five Times (' + str(i) + ')')
+    i += 1
