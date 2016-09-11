856388902306b196286c7d08c19601b4a3de8992
RaddKat <mikeradka@gmail.com>
2016-09-10 17:47:21 -0700

    Added 'helloWorld.py'

---
+++ helloWorld.py
@@ -0,0 +1,8 @@
+# Basic Hello World program
+# This program says hello and asks for my name
+# Written on Python 3.5.1
+
+print('Hello, world!')
+myName = input('What is your name? ') # ask for their name
+print('It is good to meet you, ' + myName + '.')
+print('The length of your name is: ' + str(len(myName)) + '.')
