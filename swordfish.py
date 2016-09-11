f799afd8d33bef6df355af0af48c1efa6e54bb1e
RaddKat <mikeradka@gmail.com>
2016-09-10 20:40:02 -0700

    Added swordfish.py
    This program demonstrates use of 'continue' and 'break' in a while loop

---
+++ swordfish.py
@@ -0,0 +1,11 @@
+# This program demonstrates use of 'continue' and 'break' in a while loop
+# Written on Python 3.5.1
+
+while True:
+    name = input('Who are you? ')
+    if name != 'Joe':
+        continue
+    password = input('Hello, Joe. What is the password? (It is a fish.) ')
+    if password == 'swordfish':
+        break
+print('Access granted.')
