9ec17917fe4f6f2bfc763857c4fde7d357897f7f
RaddKat <mikeradka@gmail.com>
2016-09-10 22:46:51 -0700

    Added sameName2.py
    This program demonstrates use of the global statement within a function

---
+++ ATBS/012-sameName2.py
@@ -0,0 +1,13 @@
+# This program demonstrates use of the global statement within a function
+# Because 'eggs' is declared global at the top of spam(), when eggs is set to 'spam',
+# this assignment is done to the globally scoped 'eggs'.
+# No local 'eggs' variable is created.
+# Written in Python 3.5.1
+
+def spam():
+    global eggs
+    eggs = 'spam'
+
+eggs = 'global'
+spam()
+print(eggs)
