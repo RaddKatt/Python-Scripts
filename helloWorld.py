3e3aec3df4897897735e0189c99b6b0857c07b42
RaddKat <mikeradka@gmail.com>
2016-09-10 17:59:05 -0700

    Updated output verbiage

--- helloWorld.py
+++ helloWorld.py
@@ -5,6 +5,6 @@
 print('Hello, world!')
 myName = input('What is your name? ') # asks for their name
 print('It is good to meet you, ' + myName + '.')
-print('The length of your name is: ' + str(len(myName)) + '.')
+print('The length of your name is: ' + str(len(myName)) + ' characters.')
 myAge = input('What is your age? ')# asks for their age
 print('You will be ' + str(int(myAge) + 1) + ' in a year.')
