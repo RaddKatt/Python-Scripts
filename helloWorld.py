8b8146ee42d1553a8ac8122099178955735e143b
RaddKat <mikeradka@gmail.com>
2016-09-10 17:56:19 -0700

    Added Age Feature

--- helloWorld.py
+++ helloWorld.py
@@ -3,6 +3,8 @@
 # Written on Python 3.5.1
 
 print('Hello, world!')
-myName = input('What is your name? ') # ask for their name
+myName = input('What is your name? ') # asks for their name
 print('It is good to meet you, ' + myName + '.')
 print('The length of your name is: ' + str(len(myName)) + '.')
+myAge = input('What is your age? ')# asks for their age
+print('You will be ' + str(int(myAge) + 1) + ' in a year.')
