6ffcf923ec4ec4597a9af9df1dd341b46b6405bc
RaddKat <mikeradka@gmail.com>
2016-09-10 21:30:48 -0700

    Added printRandom.py
    This program demonstrates importing Python modules.

---
+++ ATBS/006-printRandom.py
@@ -0,0 +1,8 @@
+# This program demonstrates importing Python modules
+# This program demonstrates use of the random.randint() function
+# This program generates random integers between and including 1-10, and prints them
+# Written in Python 3.5.1
+
+import random
+for i in range(5):
+    print(random.randint(1,10))
