5d12dc72a9dd0b6af2f03d2fef89864682d1b141
RaddKat <mikeradka@gmail.com>
2016-09-10 22:53:55 -0700

    Added sameName3.py
    This is another demonstration of local/global variable scoping

---
+++ ATBS/013-sameName3.py
@@ -0,0 +1,17 @@
+# Another demonstration of local/global variable scoping
+# Written in Python 3.5.1
+
+def spam():
+    global eggs
+    eggs = 'spam' # this is the global variable
+
+def bacon():
+    eggs = 'bacon' # this is a local variable
+
+def ham():
+    print(eggs) # this is the global variable
+
+eggs = 42 # this is the global variable
+
+spam()
+print(eggs)
