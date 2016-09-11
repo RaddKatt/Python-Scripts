367c607d7ba9086aa12ceaf44fc41bdf9b191070
RaddKat <mikeradka@gmail.com>
2016-09-11 11:47:29 -0700

    Added allMyCats1.py
    This program demonstrates duplicated code that could be mitigated by
    using loops w/ lists. Instead of using multiple, repetitive values like
    below, you can use a single variable that contains a List value.

---
+++ ATBS/04-Lists/allMyCats1.py
@@ -0,0 +1,15 @@
+# This program demonstrates duplicated code
+# that could be mitigated by using loops w/ lists
+# Instead of using multiple, repetitive values like below,
+# You can use a single variable that contains a List value.
+# Written in Python 3.5.1 on Mac
+
+catName1 = input('\nEnter the name of cat 1: ')
+catName2 = input('Enter the name of cat 2: ')
+catName3 = input('Enter the name of cat 3: ')
+catName4 = input('Enter the name of cat 4: ')
+catName5 = input('Enter the name of cat 5: ')
+catName6 = input('Enter the name of cat 6: ')
+
+print('\nThe cat names are: ')
+print('\t' + catName1 + ', ' + catName2 + ', ' + catName3 + ', ' + catName4 + ', ' + catName5 + ', ' + catName6 + '\n')
