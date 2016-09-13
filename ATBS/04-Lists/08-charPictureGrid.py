841deba7619e5f7859014ec729db74ef167d209f
RaddKat <mikeradka@gmail.com>
2016-09-12 21:13:36 -0700

    Fixed charPictureGrid.py
    This program demonstrates the use of nested loops to invert a nested
    list

--- ATBS/04-Lists/08-charPictureGrid.py
+++ ATBS/04-Lists/08-charPictureGrid.py
@@ -1,3 +1,5 @@
+# This program demonstrates the use of nested loops
+# to invert a nested list
 # Written in Python 3.5.1 on Mac
 
 grid = [['.','.','.','.','.','.'],
@@ -9,3 +11,21 @@ grid = [['.','.','.','.','.','.'],
 ['O','O','O','O','.','.'],
 ['.','O','O','.','.','.'],
 ['.','.','.','.','.','.']]
+
+print('\n=========')
+
+for i in range(0,len(grid)):
+    newGrid = ''
+    for j in range(0,len(grid[0])):
+        newGrid += grid[i][j]
+    print(newGrid)
+
+print('\n=========\n')
+
+for i in range(0,len(grid[0])):
+    newGrid = ''
+    for j in range(0,len(grid)):
+        newGrid += grid[j][i]
+    print(newGrid)
+
+print('\n=========\n')
