0a26f128402333cb5f31778ff09c6f090ffcb824
RaddKat <mikeradka@gmail.com>
2016-09-24 16:26:56 -0700

    Made checkMove a try/except clause

--- ATBS/05-Dictionaries/ticTacToe.py
+++ ATBS/05-Dictionaries/ticTacToe.py
@@ -3,42 +3,42 @@ This program demonstrates a simple data structure to model a game board for Tic 
 Written in Python 3.5.1 on Mac
 """
 
-import string
-
 theBoard = {
-    '1' : ' ',
-    '2' : ' ',
-    '3' : ' ',
-    '4' : ' ',
-    '5' : ' ',
-    '6' : ' ',
-    '7' : ' ',
-    '8' : ' ',
-    '9' : ' '
+    1 : ' ',
+    2 : ' ',
+    3 : ' ',
+    4 : ' ',
+    5 : ' ',
+    6 : ' ',
+    7 : ' ',
+    8 : ' ',
+    9 : ' '
 }
 
 turn = 'X'
 
 def printBoard(board):
-    print('\n' + board['1'] + '|' + board['2'] + '|' + board['3'])
+    print('\n' + board[1] + '|' + board[2] + '|' + board[3])
     print('-+-+-')
-    print(board['4'] + '|' + board['5'] + '|' + board['6'])
+    print(board[4] + '|' + board[5] + '|' + board[6])
     print('-+-+-')
-    print(board['7'] + '|' + board['8'] + '|' + board['9'] + '\n')
+    print(board[7] + '|' + board[8] + '|' + board[9] + '\n')
 
 def checkMove(move):
     while True:
         errMsg = 'Move entered must be a number between 1 and 9.'
-        if not move.isnumeric():
+        try:
+            move = int(move)            # if the entry is an integer
+            if move < 1 or move > 9:
+                print(errMsg)
+                move = input(prompt)
+                continue
+            else:
+                return move
+        except:                         # if the entry is not an integer
             print(errMsg)
             move = input(prompt)
             continue
-        elif int(move) < 1 or int(move) > 9:
-            print(errMsg)
-            move = input(prompt)
-            continue
-        else:
-            return move
 
 '''
 def checkWin(board):
