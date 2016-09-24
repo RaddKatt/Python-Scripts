b496012b9478ec210bf93f50d70ebd22d12b90a0
RaddKat <mikeradka@gmail.com>
2016-09-24 15:46:22 -0700

    Added input validation

--- ATBS/05-Dictionaries/ticTacToe.py
+++ ATBS/05-Dictionaries/ticTacToe.py
@@ -2,6 +2,7 @@
 This program demonstrates a simple data structure to model a game board for Tic Tac Toe
 Written in Python 3.5.1 on Mac
 """
+import string
 
 theBoard = {
     '1' : ' ',
@@ -22,19 +23,33 @@ def printBoard(board):
     print('-+-+-')
     print(board['7'] + '|' + board['8'] + '|' + board['9'] + '\n')
 
+def checkMove(move):
+    while True:
+        if not move.isnumeric():
+            print('Move entered must be a number between 1 and 9.')
+            move = input('Turn for ' + turn + '. Move on which space? (1-9) ')
+            continue
+        elif int(move) < 1 or int(move) > 9:
+            print('Move entered must be a number between 1 and 9.')
+            move = input('Turn for ' + turn + '. Move on which space? (1-9) ')
+            continue
+        else:
+            return move
+
 '''
 def checkWin(board):
     if
     break
 '''
 
-printBoard(theBoard)
 turn = 'X'
 for i in range(9):
-    move = input('Turn for ' + turn + '. Move on which space? ')
+    printBoard(theBoard)
+    move = input('Turn for ' + turn + '. Move on which space? (1-9) ')
+    move = checkMove(move)
+
     theBoard[move] = turn
     if turn == 'X':
         turn = 'O'
     else:
         turn = 'X'
-    printBoard(theBoard)
