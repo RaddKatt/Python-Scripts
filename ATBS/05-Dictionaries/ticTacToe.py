8d5d8945fba8f505f1edb04974ef71a789d0b74d
RaddKat <mikeradka@gmail.com>
2016-09-24 11:46:24 -0700

    Added turns
    Added the ability to take turns between ‘X’ player and ‘O’ player.

--- ATBS/05-Dictionaries/ticTacToe.py
+++ ATBS/05-Dictionaries/ticTacToe.py
@@ -4,15 +4,31 @@ Written in Python 3.5.1 on Mac
 """
 
 theBoard = {
-    'top-L' : '',
-    'top-M' : '',
-    'top-R' : '',
-    'mid-L' : '',
-    'mid-M' : '',
-    'mid-R' : '',
-    'low-L' : '',
-    'low-M' : '',
-    'low-R' : ''
+    'top-L' : ' ',
+    'top-M' : ' ',
+    'top-R' : ' ',
+    'mid-L' : ' ',
+    'mid-M' : ' ',
+    'mid-R' : ' ',
+    'low-L' : ' ',
+    'low-M' : ' ',
+    'low-R' : ' '
 }
 
-print(theBoard)
+def printBoard(board):
+    print('\n' + board['top-L'] + '|' + board['top-M'] + '|' + board['top-R'])
+    print('-+-+-')
+    print(board['mid-L'] + '|' + board['mid-M'] + '|' + board['mid-R'])
+    print('-+-+-')
+    print(board['low-L'] + '|' + board['low-M'] + '|' + board['low-R'] + '\n')
+
+printBoard(theBoard)
+turn = 'X'
+for i in range(9):
+    move = input('Turn for ' + turn + '. Move on which space? ')
+    theBoard[move] = turn
+    if turn == 'X':
+        turn = 'O'
+    else:
+        turn = 'X'
+    printBoard(theBoard)
