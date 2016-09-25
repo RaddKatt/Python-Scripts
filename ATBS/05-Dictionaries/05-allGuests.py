fc028fb1752d6d2b87b7e4eb5a94f8928d5f5844
RaddKat <mikeradka@gmail.com>
2016-09-24 18:49:59 -0700

    Added 'allGuests.py'

---
+++ ATBS/05-Dictionaries/05-allGuests.py
@@ -0,0 +1,12 @@
+"""
+This program demonstrates the use of nested dictionariees
+Written in Python 3.5.1 on Mac
+"""
+
+allGuests = {
+    'Alice' : {'apples' : 5, 'pretzels' : 12},
+    'Bob' : {'ham sandwiches' : 3, 'apples' : 2},
+    'Carol' : {'cups' : 3, 'apple pies' : 1}
+}
+
+print(allGuests)
