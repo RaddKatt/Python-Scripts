216e8f4faef0daff7d0b6c670df91a374fd8b5e0
RaddKat <mikeradka@gmail.com>
2016-09-24 19:02:18 -0700

    Added totalBrought

--- ATBS/05-Dictionaries/05-allGuests.py
+++ ATBS/05-Dictionaries/05-allGuests.py
@@ -9,4 +9,15 @@ allGuests = {
     'Carol' : {'cups' : 3, 'apple pies' : 1}
 }
 
-print(allGuests)
+def totalBrought(guests,item):
+    numBrought = 0
+    for k, v in guests.items():
+        numBrought = numBrought + v.get(item, 0)
+    return numBrought
+
+print('\nNumber of things being brought:')
+print('- Apples = ' + str(totalBrought(allGuests,'apples')))
+print('- Cups = ' + str(totalBrought(allGuests,'cups')))
+print('- Cakes = ' + str(totalBrought(allGuests,'cakes')))
+print('- Ham Sandwiches = ' + str(totalBrought(allGuests,'ham sandwiches')))
+print('- Apple Pies = ' + str(totalBrought(allGuests,'apple pies')) + '\n')
